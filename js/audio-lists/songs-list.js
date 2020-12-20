b5f6a78d03a2ee029a021396504154285b5e6968
Akhil Goyal <akhilgoyal@MacBook-Pro.local>
2020-12-20 13:35:45 -0500

    Replaced the current music with copyright free music.

--- js/audio-lists/songs-list.js
+++ js/audio-lists/songs-list.js
@@ -1,53 +1,65 @@
-let BASE_URL_SONGS = './resources/audio/music/songs/eng';
-let BASE_URL_IMAGES = './resources/audio/music/images/eng';
+let BASE_URL_SONGS = './resources/audio/music/songs';
+let BASE_URL_IMAGES = './resources/audio/music/images';
 
 export const musicLibrary = [
     {
-        name: "God is a woman",
-        path: `${BASE_URL_SONGS}/1.mp3`,
-        img: `${BASE_URL_IMAGES}/1.jpg`,
-        singer: "Ariana Grande"
+        name: "The Lounge",
+        path: `${BASE_URL_SONGS}/thelounge.mp3`,
+        img: `${BASE_URL_IMAGES}/thelounge.jpg`,
+        singer: "Loungeman Greyham"
     },
     {
-        name: "Just the way you are",
-        path: `${BASE_URL_SONGS}/2.mp3`,
-        img: `${BASE_URL_IMAGES}/2.png`,
-        singer: "Bruno Mars"
+        name: "Creepy",
+        path: `${BASE_URL_SONGS}/creepy.mp3`,
+        img: `${BASE_URL_IMAGES}/creepy.jpg`,
+        singer: "Dr. Creep X"
     },
     {
-        name: "Song 3",
-        path: `${BASE_URL_SONGS}/1.mp3`,
-        img: `${BASE_URL_IMAGES}/1.jpg`,
-        singer: "Artist 3"
+        name: "Energy",
+        path: `${BASE_URL_SONGS}/energy.mp3`,
+        img: `${BASE_URL_IMAGES}/energy.jpg`,
+        singer: "Popeye Sailorman"
     },
     {
-        name: "Song 4",
-        path: `${BASE_URL_SONGS}/2.mp3`,
-        img: `${BASE_URL_IMAGES}/2.png`,
-        singer: "Artist 4"
+        name: "Epic",
+        path: `${BASE_URL_SONGS}/epic.mp3`,
+        img: `${BASE_URL_IMAGES}/epic.jpg`,
+        singer: "Thorin Oakenshield"
     },
     {
-        name: "God is a woman",
-        path: `${BASE_URL_SONGS}/1.mp3`,
-        img: `${BASE_URL_IMAGES}/1.jpg`,
-        singer: "Ariana Grande"
+        name: "Erf",
+        path: `${BASE_URL_SONGS}/erf.mp3`,
+        img: `${BASE_URL_IMAGES}/erf.jpg`,
+        singer: "Dobby the Elf"
     },
     {
-        name: "Just the way you are",
-        path: `${BASE_URL_SONGS}/2.mp3`,
-        img: `${BASE_URL_IMAGES}/2.png`,
-        singer: "Bruno Mars"
+        name: "Groovy HipHop",
+        path: `${BASE_URL_SONGS}/groovyhiphop.mp3`,
+        img: `${BASE_URL_IMAGES}/groovyhiphop.jpg`,
+        singer: "Kanye East"
     },
     {
-        name: "Song 3",
-        path: `${BASE_URL_SONGS}/1.mp3`,
-        img: `${BASE_URL_IMAGES}/1.jpg`,
-        singer: "Artist 3"
+        name: "Moose",
+        path: `${BASE_URL_SONGS}/moose.mp3`,
+        img: `${BASE_URL_IMAGES}/moose.jpg`,
+        singer: "Gellert Grindelwald"
     },
     {
-        name: "Song 4",
-        path: `${BASE_URL_SONGS}/2.mp3`,
-        img: `${BASE_URL_IMAGES}/2.png`,
-        singer: "Artist 4"
+        name: "Once again",
+        path: `${BASE_URL_SONGS}/onceagain.mp3`,
+        img: `${BASE_URL_IMAGES}/onceagain.jpg`,
+        singer: "Samwell Tarly"
+    },
+    {
+        name: "Psychedelic",
+        path: `${BASE_URL_SONGS}/psychedelic.mp3`,
+        img: `${BASE_URL_IMAGES}/psychedelic.jpg`,
+        singer: "Trippy Jay"
+    },
+    {
+        name: "Sunny",
+        path: `${BASE_URL_SONGS}/sunny.mp3`,
+        img: `${BASE_URL_IMAGES}/sunny.jpg`,
+        singer: "Bugs Bunny"
     },
 ];
